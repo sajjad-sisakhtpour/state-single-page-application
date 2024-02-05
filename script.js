a0c22b9376728db2de4126f60f973be06bc9f67b
sajjad <sajjad.sisakhtpour@gmail.com>
2024-02-05 05:32:55 +0330

    created single page application

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 document.querySelector("nav").addEventListener("click", (e) => {
   if (e.target.nodeName != "I") return;
   console.log(e.target);
+  let data;
+  switch (e.target.getAttributeNode("item-id").value) {
+    case "home":
+      data = { title: "home", color: "red", itemId: "home" };
+      update(data);
+      break;
+    case "search":
+      data = { title: "search", color: "blue", itemId: "search" };
+      update(data);
+      break;
+    case "likes":
+      data = { title: "likes", color: "green", itemId: "likes" };
+      update(data);
+      break;
+    case "profile":
+      data = { title: "profile", color: "pink", itemId: "profile" };
+      update(data);
+      break;
+  }
 });
+
+function update(data) {
+  document.querySelector("h1").innerText = data.title;
+  document.querySelector("main").style.backgroundColor = data.color;
+  document.querySelectorAll("I").forEach((Element) => {
+    Element.style.color = "black";
+  });
+  document.querySelector(`I[item-id=${data.itemId}]`).style.color = data.color;
+}
